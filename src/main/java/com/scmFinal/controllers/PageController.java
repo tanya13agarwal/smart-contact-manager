2b4e97e01ec1bb0027f33902157e4f34fe8859d9
Tanya Agarwal <jiya03tanya@gmail.com>
2024-10-01 00:34:33 +0530

    added about and services page

--- src/main/java/com/scmFinal/controllers/PageController.java
+++ src/main/java/com/scmFinal/controllers/PageController.java
@@ -7,17 +7,29 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class PageController {
     // is route p click krke ye controller call hoga
+    // Home Route
     @RequestMapping("/home")
     public String home(Model model){
-        
         System.out.println("Home Page Handler");
-        
         // sending data to view
         model.addAttribute("name", "Tanya Agarwal"); // now we add name , value of attribute 
         model.addAttribute("youtubeChannel", "hey yaa tanya"); // now we add name , value of attribute 
         model.addAttribute("googleLink", "https://www.google.com"); // now we add name , value of attribute 
-
-
         return "home";
     }
+
+    // About Route
+    @RequestMapping("/about")
+    public String about(Model model){
+        System.out.println("About page loading");
+        return "about";
+    }
+
+    // Services Route
+    @RequestMapping("/services")
+    public String servicesPage(Model model){
+        System.out.println("Services page loading");
+        return "services";
+    }
+
 }
